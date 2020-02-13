faf8933145a646d138ba56c3870827f51379a8ca
dgsilva <diegorj93@gmail.com>
2020-02-12 23:30:13 -0300

    Foi comentada as classes

--- projetoService/src/main/java/main/Aluno.java
+++ projetoService/src/main/java/main/Aluno.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table(name="aluno")
+//Classe Aluno
 public class Aluno implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
